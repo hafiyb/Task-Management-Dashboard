b1efd35067958010c6fb2cb42668f36a5cc71ebe
hafiyb <hafiyb@gmail.com>
2024-01-11 10:11:06 +0800

    feat: setup storybook & tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/stories/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
@@ -13,6 +14,25 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      offWhite: '#F8F9FB',
+      grayBackground: '#ECEFF4',
+      blueMain: '#8BB2F2',
+      blueHover: '#7DA0D9',
+      redMain: '#F06464',
+      grayText: '#666666',
+      darkText: '#333333',
+    },
+    height: {
+      navbar: '64px',
+      screen: 'calc(100vh - 64px)',
+    },
+    width: {
+      sidebar: '64px',
+    },
+    // minWidth:{
+    //   sidebar: '64px',
+    // },
+    plugins: [],
   },
-  plugins: [],
-}
+};
